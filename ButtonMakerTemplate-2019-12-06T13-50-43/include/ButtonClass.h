4456a1981e8f1fadf2bef28f46bd25411e58bc32
asimonson1125 <asimonson1125@users.noreply.github.com>
2019-12-07 16:37:53 -0500

    Added font changing capability
    added structure for font changing, but not in constructors
    
    cured chronic 'this->' sickness, who knows what my problem was
    
    Updated readme to reflect changes

--- ButtonMakerTemplate-2019-12-06T13-50-43/include/ButtonClass.h
+++ ButtonMakerTemplate-2019-12-06T13-50-43/include/ButtonClass.h
@@ -12,10 +12,35 @@
 #include "vex.h"
 
 
-void insertText(std::string text, int width, int height, int xi, int yi){
+void insertText(std::string text, int width, int height, int xi, int yi, int Ftype, int Fsize){
   int x;
   int y;
   const char *string = text.c_str();
+  switch(Ftype){
+    case 0: switch(Fsize){
+      case 12: Brain.Screen.setFont(mono12);
+      case 15: Brain.Screen.setFont(mono15);
+      case 20: Brain.Screen.setFont(mono20);
+      case 30: Brain.Screen.setFont(mono30);
+      case 40: Brain.Screen.setFont(mono40);
+      break;
+    };
+
+    case 1: switch(Fsize){
+      case 20: Brain.Screen.setFont(prop20);
+      case 30: Brain.Screen.setFont(prop30);
+      case 40: Brain.Screen.setFont(prop40);
+      case 60: Brain.Screen.setFont(prop60);
+      break;
+    };
+
+    case 2: switch(Fsize){
+      //case 15: Brain.Screen.setFont(cjk16);
+      break;
+    };
+  }
+//WHAT IS MONOXL I DID NOT SIGN UP FOR THIS
+
   x = xi - (vexDisplayStringWidthGet(string)/2);
   y = yi + (vexDisplayStringHeightGet(string)/4);
   Brain.Screen.printAt(x, y, string);
@@ -27,17 +52,21 @@ int nextId = 0;
 
 class lcdButton {
   public:
+  int buttonId;
+
   int xPos = 100;
   int yPos = 100;
   int width = 20;
   int height = 20;
-  int buttonId;
+  int hue = 0;
+  int font = 0;
+  int fontsize = 10;
   std::string text = "";
+
   int xMin;
   int xMax;
   int yMin;
   int yMax;
-  int hue = 0;
   
   //_________________________________________________________________________
   //_________________________________________________________________________
@@ -132,19 +161,19 @@ class lcdButton {
   //_________________________________________________________________________
 
   void moveTo(int x, int y){
-    this->xPos = x;
-    this->yPos = y;
+    xPos = x;
+    yPos = y;
     draw();
   }
 
   void setText(std::string chars){
-    this->text = chars;
+    text = chars;
     draw();
   }
 
   void setSize(int tall, int width){
-    this->height = tall;
-    this->width = width;
+    height = tall;
+    width = width;
     draw();
   }
 
@@ -153,13 +182,21 @@ class lcdButton {
     draw();
   }
 
+  void setFont(int chosen){
+    font = chosen;
+  }
+
+  void setFontSize(int chosen){
+    fontsize = chosen;
+  }
+
   void draw(){
     xMin = xPos - (width/2);
     xMax = xPos + (width/2);
     yMin = yPos - (height/2);
     yMax = yPos + (height/2);
     Brain.Screen.drawRectangle(xPos - (width/2), yPos - (height/2), width, height, hue);
-    insertText(text, width, height, xPos, yPos);
+    insertText(text, width, height, xPos, yPos, font, fontsize);
   }
 
 //________________________________________________________________________________________________
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ### This is a WORK IN PROGRESS, expect the unexpected
 
 The VEXcode Button Generator consists of a class designed to automate the process of adding a button to the V5 Brain screen in VEXcode.  It is meant to help new users get started with the V5 system, not as a tool for advanced porgrammers.  It can, however, be a handy tool for quickly making autonomous selectors or control the robot from the brain itself.
+With it you can create a button with automatically centered text and press detection in one line and change button attributes on the fly.
 
 ___
 
@@ -12,16 +13,20 @@ ___
 
 ___
 
-### Capabilities:
 
-In one line you can create a button with automatically centered text and press detection
+###Button attributes:
+ - centered position in coordinate plane
+ - button color
+ - button dimensions
+ - button color (hue)
+ - text inside button
+ - text font
+ - text size
 
-Additionally, change button position, size, color, and text after initial creation.
 
 WIP:
  - use custom images from SD card
  - redraw button automatically after parameter change
- - font and font size changes
 
 ### Limitations:
 Press detection is limited to rectangular shapes, it cannot accurately detect presses for other shapes.
